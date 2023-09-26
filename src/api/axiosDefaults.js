6da45e63297cf395f9e7916ca30f211b1199db09
Ala Yousef <alaa_shlbaieh@hotmail.com>
2023-09-26 15:25:41 +0000

    add navlinks for signed in users

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
-axios.defaults.baseURL="https://moment-alaa-ce3e92901a21.herokuapp.com";
-axios.defaults.headers.post['Content-type'] = 'multipart/form-data';
-axios.defaults.withCredentials=true
+axios.defaults.baseURL = "https://apidjango-a1b0ccf0d7a8.herokuapp.com/";
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
